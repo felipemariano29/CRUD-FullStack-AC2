850258af172e70651e076a53863966bc5a7206b3
Felipe Mariano <86112326+felipemariano29@users.noreply.github.com>
2024-05-20 13:05:56 -0300

    Created User model

---
+++ backend/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["FE", "BE", "DATA", "LEAD"], required: true },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(this.password, salt);
+
+  this.password = hash;
+
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;

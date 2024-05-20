b20508daf93ac7a7070747357440b4e0f440621e
Felipe Mariano <86112326+felipemariano29@users.noreply.github.com>
2024-05-20 13:38:02 -0300

    Created User routes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  getUserCountByRole,
+} = require("../controllers/userController");
+const { auth } = require("../middlewares/authMiddleware");
+const router = express.Router();
+
+router.get("/", auth, getUsers); // get all users
+router.get("/:id", auth, getUser); // get a single user
+router.post("/", auth, createUser); // create a new user
+router.put("/:id", auth, updateUser); // update a user
+router.delete("/:id", auth, deleteUser); // delete a user
+router.get("/count", auth, getUserCountByRole); // get user count by role
+
+module.exports = router;
